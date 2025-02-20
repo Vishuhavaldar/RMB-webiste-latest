0a734d58e6783de895649e64eb0d707926f53f66
Vishwanath Havaldar <vishuhavaldar@gmail.com>
2025-02-20 22:21:12 +0530

    basic features added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+;
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule,  HeaderComponent,FooterComponent], // Add RouterModule
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Corrected `styleUrls`
 })
 export class AppComponent {
   title = 'RMB';
--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,15 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering()
+    ,provideClientHydration() ,
+    provideRouter(routes)
   ]
 };
 
